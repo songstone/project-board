c3272f5707aeec4e28c76df8b5276c097369b99a
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-09-07 16:50:57 +0900

    feat: #10 - 기존 도메인 엔티티로 전환
    - Article, ArticleComment 엔티티로 변경

--- src/main/java/com/song/projectboard/domain/ArticleComment.java
+++ src/main/java/com/song/projectboard/domain/ArticleComment.java
@@ -1,14 +1,57 @@
 package com.song.projectboard.domain;
 
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+    @Index(columnList = "content"),
+    @Index(columnList = "createdAt"),
+    @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class ArticleComment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "ARTICLE_ID")
     private Article article;
+
+    @Setter
+    @Column(nullable = false, length = 500)
     private String content;
 
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+    public ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+    public static ArticleComment of(Article article, String comment) {
+        return new ArticleComment(article, comment);
+    }
 }
