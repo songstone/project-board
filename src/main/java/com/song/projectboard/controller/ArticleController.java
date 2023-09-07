309d8c751cef765132548688149a66827ac2856f
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-09-08 05:39:04 +0900

    feat: #16 - 게시글 뷰 기본 작성
    - detail.html 기본 골격 구조 작성

--- src/main/java/com/song/projectboard/controller/ArticleController.java
+++ src/main/java/com/song/projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.song.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,6 +15,15 @@ public class ArticleController {
     @GetMapping
     public String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
+
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(ModelMap map, @PathVariable Long articleId) {
+        map.addAttribute("article", null);
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
     }
 }
