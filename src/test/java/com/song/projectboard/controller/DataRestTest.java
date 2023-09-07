a13502ebecc3c93ab2822973ad9f319086ad6994
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-09-08 00:59:42 +0900

    test: #5 - data rest api 확인용 테스트
    - Data Rest 사용으로 편의상 @WebMvcTest 대신 @AutoConfigureMockMvc, @SpringBootTest 이용한 통합 테스트 형태로 작성

---
+++ src/test/java/com/song/projectboard/controller/DataRestTest.java
@@ -0,0 +1,68 @@
+package com.song.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data Rest 테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+//@WebMvcTest
+public class DataRestTest {
+
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    void articles_list_test() throws Exception {
+        mvc.perform(get("/api/articles"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 게시글 단건 조회")
+    @Test
+    void articles_select_test() throws Exception {
+        mvc.perform(get("/api/articles/1"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 게시글의 댓글 리스트 조회")
+    @Test
+    void articles_articleComments_list_test() throws Exception {
+        mvc.perform(get("/api/articles/1/articleComments"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 리스트 조회")
+    @Test
+    void articleComments_list_test() throws Exception {
+        mvc.perform(get("/api/articleComments"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 단건 조회")
+    @Test
+    void articleComments_select_test() throws Exception {
+        mvc.perform(get("/api/articleComments/1"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+}
