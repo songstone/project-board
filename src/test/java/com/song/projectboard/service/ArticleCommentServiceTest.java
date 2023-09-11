6c3461234afd3d0cc0a25f680171129fe1ac5c3a
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-09-11 23:14:56 +0900

    test: #23 - 게시글, 댓글 테스트 정의

--- src/test/java/com/song/projectboard/service/ArticleCommentServiceTest.java
+++ src/test/java/com/song/projectboard/service/ArticleCommentServiceTest.java
@@ -4,9 +4,9 @@ import com.song.projectboard.domain.Article;
 import com.song.projectboard.domain.ArticleComment;
 import com.song.projectboard.domain.UserAccount;
 import com.song.projectboard.dto.ArticleCommentDto;
+import com.song.projectboard.dto.UserAccountDto;
 import com.song.projectboard.repository.ArticleCommentRepository;
 import com.song.projectboard.repository.ArticleRepository;
-import com.song.projectboard.repository.UserAccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,12 +16,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.*;
 
 @DisplayName("게시글 댓글 로직 테스트")
 @ExtendWith(MockitoExtension.class)
@@ -37,30 +35,126 @@ class ArticleCommentServiceTest {
 
     @DisplayName("게시글 댓글 리스트")
     @Test
-    void searchArticleComments() {
-
+    void search_article_articleComments() {
+        //given
         Long articleId = 1L;
+        ArticleComment expected = createArticleComment("content");
+        given(articleCommentRepository.findByArticle_Id(articleId)).willReturn(List.of(expected));
 
-        given(articleRepository.findById(articleId)).willReturn(
-            Optional.of(Article.of(null,"title", "content", "#hashtag")));
+        //when
+        List<ArticleCommentDto> actual = articleCommentService.searchArticleComments(articleId);
 
-        List<ArticleCommentDto> articleComments = articleCommentService.searchArticleComments(articleId);
-
-        assertThat(articleComments).isNotNull();
-        then(articleRepository).should().findById(articleId);
+        //then
+        assertThat(actual)
+            .hasSize(1)
+            .first().hasFieldOrPropertyWithValue("content", expected.getContent());
+        then(articleCommentRepository).should().findByArticle_Id(articleId);
+        ;
     }
+
     @DisplayName("댓글 등록")
     @Test
-    void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment() {
+    void save_articleComment() {
         // Given
+        ArticleCommentDto dto = createArticleCommentDto("댓글");
+        given(articleRepository.getReferenceById(dto.articleId())).willReturn(createArticle());
         given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
 
         // When
-        articleCommentService.saveArticleComment(ArticleCommentDto.of("comment", LocalDateTime.now(), "song", LocalDateTime.now(), "song"));
+        articleCommentService.saveArticleComment(dto);
 
         // Then
+        then(articleRepository).should().getReferenceById(dto.articleId());
         then(articleCommentRepository).should().save(any(ArticleComment.class));
     }
 
+    @DisplayName("댓글 수정")
+    @Test
+    void update_articleComment() {
+        //given
+        String oldContent = "content";
+        String updatedContent = "댓글";
+        ArticleComment articleComment = createArticleComment(oldContent);
+        ArticleCommentDto dto = createArticleCommentDto(updatedContent);
+        given(articleCommentRepository.getReferenceById(dto.id())).willReturn(articleComment);
 
+        //when
+        articleCommentService.updateArticleComment(dto);
+
+        //then
+        assertThat(articleComment.getContent())
+            .isNotEqualTo(oldContent)
+            .isEqualTo(updatedContent);
+        then(articleCommentRepository).should().getReferenceById(dto.id());
+    }
+
+    @DisplayName("댓글 삭제")
+    @Test
+    void delete_articleComment() {
+        //given
+        Long articleCommentId = 1L;
+        willDoNothing().given(articleCommentRepository).deleteById(articleCommentId);
+
+        //when
+        articleCommentService.deleteArticleComment(articleCommentId);
+
+        //then
+        then(articleCommentRepository).should().deleteById(articleCommentId);
+    }
+
+
+    private ArticleCommentDto createArticleCommentDto(String content) {
+        return ArticleCommentDto.of(
+            1L,
+            1L,
+            createUserAccountDto(),
+            content,
+            LocalDateTime.now(),
+            "song",
+            LocalDateTime.now(),
+            "song"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+            1L,
+            "song",
+            "password",
+            "song@mail.com",
+            "Song",
+            "This is memo",
+            LocalDateTime.now(),
+            "song",
+            LocalDateTime.now(),
+            "song"
+        );
+    }
+
+    private ArticleComment createArticleComment(String content) {
+        return ArticleComment.of(
+            Article.of(createUserAccount(), "title", "content", "hashtag"),
+            createUserAccount(),
+            content
+        );
+    }
+
+    private UserAccount createUserAccount() {
+        return UserAccount.of(
+            "song",
+            "password",
+            "song@email.com",
+            "Song",
+            null
+        );
+    }
+
+    private Article createArticle() {
+        return Article.of(
+            createUserAccount(),
+            "title",
+            "content",
+            "#java"
+        );
+    }
 }
--- src/test/java/com/song/projectboard/service/ArticleServiceTest.java
+++ src/test/java/com/song/projectboard/service/ArticleServiceTest.java
@@ -1,9 +1,11 @@
 package com.song.projectboard.service;
 
 import com.song.projectboard.domain.Article;
+import com.song.projectboard.domain.UserAccount;
 import com.song.projectboard.domain.type.SearchType;
 import com.song.projectboard.dto.ArticleDto;
-import com.song.projectboard.dto.ArticleUpdateDto;
+import com.song.projectboard.dto.ArticleWithCommentsDto;
+import com.song.projectboard.dto.UserAccountDto;
 import com.song.projectboard.repository.ArticleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,10 +14,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
 
@@ -30,38 +36,83 @@ class ArticleServiceTest {
     private ArticleRepository articleRepository;
 
 
+    @DisplayName("키워드 없이 게시글 검색 -> 게시글 리스트 페이지네이션 반환")
+    @Test
+    void no_keyword_searching_articles() {
+        //given
+        Pageable pageable = Pageable.ofSize(20);
+        given(articleRepository.findAll(pageable)).willReturn(Page.empty());
+
+        //when
+        Page<ArticleDto> articles = articleService.searchArticles(null, null, pageable);
+
+        //then
+        assertThat(articles).isEmpty();
+        then(articleRepository).should().findAll(pageable);
+    }
+
     @DisplayName("게시글 검색 -> 게시글 리스트 페이지네이션 반환")
     @Test
     void searching_articles() {
-        //given
+        // given
+        SearchType searchType = SearchType.TITLE;
+        String searchKeyword = "title";
+        Pageable pageable = Pageable.ofSize(20);
+        given(articleRepository.findByTitle(searchKeyword, pageable)).willReturn(Page.empty());
 
-        //when
-        Page<ArticleDto> articles = articleService.searchArticles(SearchType.TITLE, "keyword");
+        // When
+        Page<ArticleDto> articles = articleService.searchArticles(searchType, searchKeyword, pageable);
 
-        //then
-        assertThat(articles).isNotNull();
+        // Then
+        assertThat(articles).isEmpty();
+        then(articleRepository).should().findByTitle(searchKeyword, pageable);
     }
 
     @DisplayName("게시글 조회 -> 게시글 반환")
     @Test
     void select_article() {
         //given
+        Long articleId = 1L;
+        Article article = createArticle();
+        given(articleRepository.findById(articleId)).willReturn(Optional.of(article));
 
         //when
-        ArticleDto article = articleService.searchArticle(1L);
+        ArticleWithCommentsDto dto = articleService.getArticle(articleId);
 
         //then
-        assertThat(article).isNotNull();
+        assertThat(dto)
+            .hasFieldOrPropertyWithValue("title", article.getTitle())
+            .hasFieldOrPropertyWithValue("content", article.getContent())
+            .hasFieldOrPropertyWithValue("hashtag", article.getHashtag());
+        then(articleRepository).should().findById(articleId);
+    }
+
+    @DisplayName("게시글 조회 -> 게시글 없을 시 예외 발생")
+    @Test
+    void select_article_exception() {
+        // Given
+        Long articleId = 0L;
+        given(articleRepository.findById(articleId)).willReturn(Optional.empty());
+
+        // When
+        Throwable t = catchThrowable(() -> articleService.getArticle(articleId));
+
+        // Then
+        assertThat(t)
+            .isInstanceOf(EntityNotFoundException.class)
+            .hasMessage("게시글이 없습니다 - articleId: " + articleId);
+        then(articleRepository).should().findById(articleId);
     }
 
     @DisplayName("게시글 생성")
     @Test
-    void createArticle() {
+    void create_article() {
         //given
-        given(articleRepository.save(any(Article.class))).willReturn(any(Article.class));
+        ArticleDto dto = createArticleDto();
+        given(articleRepository.save(any(Article.class))).willReturn(createArticle());
 
         //when
-        articleService.saveArticle(ArticleDto.of("title", "content", "#hashtag", LocalDateTime.now(), "song"));
+        articleService.saveArticle(dto);
 
         //then
         then(articleRepository).should().save(any(Article.class));
@@ -71,25 +122,83 @@ class ArticleServiceTest {
     @Test
     void updateArticle() {
         //given
-        given(articleRepository.save(any(Article.class))).willReturn(any(Article.class));
+        Article article = createArticle();
+        ArticleDto dto = createArticleDto("new title", "new content", "#tag");
+        given(articleRepository.getReferenceById(dto.id())).willReturn(article);
 
         //when
-        articleService.updateArticle(ArticleUpdateDto.of("title", "content", "#hashtag"));
+        articleService.updateArticle(dto);
 
         //then
-        then(articleRepository).should().save(any(Article.class));
+        assertThat(article)
+            .hasFieldOrPropertyWithValue("title", dto.title())
+            .hasFieldOrPropertyWithValue("content", dto.content())
+            .hasFieldOrPropertyWithValue("hashtag", dto.hashtag());
+        then(articleRepository).should().getReferenceById(dto.id());
     }
 
     @DisplayName("게시글 삭제")
     @Test
     void deleteArticle() {
         //given
-        willDoNothing().given(articleRepository).delete(any(Article.class));
+        Long articleId = 1L;
+        willDoNothing().given(articleRepository).deleteById(articleId);
 
         //when
         articleService.deleteArticle(1L);
 
         //then
-        then(articleRepository).should().delete(any(Article.class));
+        then(articleRepository).should().deleteById(articleId);
+    }
+
+
+    private UserAccount createUserAccount() {
+        return UserAccount.of(
+            "song",
+            "password",
+            "song@email.com",
+            "Song",
+            null
+        );
+    }
+
+    private Article createArticle() {
+        return Article.of(
+            createUserAccount(),
+            "title",
+            "content",
+            "#java"
+        );
+    }
+
+    private ArticleDto createArticleDto() {
+        return createArticleDto("title", "content", "#java");
+    }
+
+    private ArticleDto createArticleDto(String title, String content, String hashtag) {
+        return ArticleDto.of(1L,
+            createUserAccountDto(),
+            title,
+            content,
+            hashtag,
+            LocalDateTime.now(),
+            "song",
+            LocalDateTime.now(),
+            "song");
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+            1L,
+            "song",
+            "password",
+            "song@mail.com",
+            "Song",
+            "This is memo",
+            LocalDateTime.now(),
+            "song",
+            LocalDateTime.now(),
+            "song"
+        );
     }
 }
