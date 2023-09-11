4c522997f3e0574ea8ee1f7cd140993a99b57535
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-09-12 04:31:59 +0900

    feat: #25 - 게시글 컨트롤러 구현
    - 게시글 리스트 구현
    - 게시글 상세조회 구현

--- src/test/java/com/song/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/song/projectboard/controller/ArticleControllerTest.java
@@ -1,14 +1,28 @@
 package com.song.projectboard.controller;
 
 import com.song.projectboard.config.SecurityConfig;
+import com.song.projectboard.dto.ArticleDto;
+import com.song.projectboard.dto.ArticleWithCommentsDto;
+import com.song.projectboard.dto.UserAccountDto;
+import com.song.projectboard.service.ArticleService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -19,6 +33,9 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
+    @MockBean
+    private ArticleService articleService;
+
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
@@ -27,6 +44,7 @@ class ArticleControllerTest {
     @Test
     void articles_list_view() throws Exception {
         //given
+        given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
 
         //when & then
         mvc.perform(get("/articles"))
@@ -34,20 +52,56 @@ class ArticleControllerTest {
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/index"))
             .andExpect(model().attributeExists("articles"));
+
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지")
     @Test
     void articles_select_view() throws Exception {
         //given
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentDto());
 
         //when & then
-        mvc.perform(get("/articles/1"))
+        mvc.perform(get("/articles/" + articleId))
             .andExpect(status().isOk())
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/detail"))
             .andExpect(model().attributeExists("article"))
             .andExpect(model().attributeExists("articleComments"));
+
+        then(articleService).should().getArticle(articleId);
+    }
+
+    private ArticleWithCommentsDto createArticleWithCommentDto() {
+        return ArticleWithCommentsDto.of(
+            1L,
+            createUserAccountDto(),
+            Set.of(),
+            "title",
+            "content",
+            "#tag1",
+            LocalDateTime.now(),
+            "song",
+            LocalDateTime.now(),
+            "song"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+            1L,
+            "song",
+            "song1234",
+            "song@email.com",
+            "Song",
+            "bla bla bla",
+            LocalDateTime.now(),
+            "song",
+            LocalDateTime.now(),
+            "song"
+        );
     }
 
 }
