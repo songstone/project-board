b8ace2debfd065af0f8053bf17691e55dce5a3d2
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-10-02 02:43:35 +0900

    feat: #37 - 댓글 기능 구현
    - 댓글 등록
    - 댓글 삭제

---
+++ src/main/java/com/song/projectboard/dto/request/ArticleCommentRequest.java
@@ -0,0 +1,19 @@
+package com.song.projectboard.dto.request;
+
+import com.song.projectboard.dto.ArticleCommentDto;
+import com.song.projectboard.dto.UserAccountDto;
+
+public record ArticleCommentRequest(Long articleId, String content) {
+
+    public static ArticleCommentRequest of(Long articleId, String content) {
+        return new ArticleCommentRequest(articleId, content);
+    }
+
+    public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
+        return ArticleCommentDto.of(
+            articleId,
+            userAccountDto,
+            content
+        );
+    }
+}
